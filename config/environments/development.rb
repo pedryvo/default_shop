9134e842efc348b56d8332f983bdb50fc36d344f
Pedro Ivo <pedryvo@gmail.com>
2022-01-11 17:13:19 -0300

    foreman logs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   
+  # Foreman logs
+  $stdout.sync = true
 
   # Devise mailer config
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
