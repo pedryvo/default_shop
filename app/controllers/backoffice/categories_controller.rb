a4f55af96986eed7208c595474938ee3386b14cd
Pedro Ivo <pedryvo@gmail.com>
2022-01-12 20:28:20 -0300

    create category form

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -5,11 +5,17 @@ class Backoffice::CategoriesController < BackofficeController
   end
 
   def new
-    
+    @category = Category.new
   end
 
   def create
-    
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to backoffice_categories_path, notice: "A categoria '#{@category.description}' foi criada com sucesso."
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +24,11 @@ class Backoffice::CategoriesController < BackofficeController
 
   def update
     
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:description)
   end
 end
