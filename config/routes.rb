15947220d96d88df8f9e33fdf20b1885dd1c8b96
Pedro Ivo <pedryvo@gmail.com>
2022-01-12 13:59:59 -0300

    adjust backoffice layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   namespace :site do
-  get 'home/index'
+    get 'home', to: 'home#index'
   end
 
   namespace :backoffice do
-  get 'dashboard/index'
+    get 'dashboard', to: 'dashboard#index'
   end
 
   devise_for :admins
   devise_for :members
+  
   root 'site/home#index'
 end
