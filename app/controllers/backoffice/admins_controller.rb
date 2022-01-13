b7f7ef1e3bf8738546b1da71bef637a9db2cb8f4
Pedro Ivo <pedryvo@gmail.com>
2022-01-12 23:09:53 -0300

    draft backoffice admin controller

--- app/controllers/backoffice/admins_controller.rb
+++ app/controllers/backoffice/admins_controller.rb
@@ -1,4 +1,43 @@
-class Backoffice::AdminsController < BackofficeController
+class Backoffice::CategoriesController < BackofficeController
+  before_action :set_admin, only: [:edit, :update]
+
+
   def index
+    @categories = Admin.all
+  end
+
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(admin_params)
+
+    if @admin.save
+      redirect_to backoffice_admins_path, notice: "O admin #{@admin.email} foi criado com sucesso."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @admin.update(admin_params)
+      redirect_to backoffice_admins_path, notice: "O admin (#{@admin.email}) foi editada com sucesso."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def admin_params
+    params.require(:admin).permit(:email)
+  end
+
+  def set_admin
+    @admin = Admin.find(params[:id])
   end
 end
