aaaf1bac9aa4d4d3b25a70109b62bbc2b5c93734
Pedro Ivo <pedryvo@gmail.com>
2022-01-16 19:04:40 -0300

    add radio button helper

--- app/helpers/backoffice/admins_helper.rb
+++ app/helpers/backoffice/admins_helper.rb
@@ -1,2 +1,14 @@
 module Backoffice::AdminsHelper
+  OptionsForRoles = Struct.new(:id, :description)
+
+  def options_for_roles
+    options = []
+    Admin.roles.each do |key, value|
+      options.push(OptionsForRoles.new(key, value))
+    end
+
+    options.each {|option| option.description == 0 ? option.description = 'Acesso completo' : option.description = 'Acesso Restrito' }
+
+    options
+  end
 end
