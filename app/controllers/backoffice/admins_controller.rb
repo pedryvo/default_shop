e6f895c1119e1468bc5d0fa256f430901225ead3
Pedro Ivo <pedryvo@gmail.com>
2022-01-17 09:56:49 -0300

    add policy options to admins

--- app/controllers/backoffice/admins_controller.rb
+++ app/controllers/backoffice/admins_controller.rb
@@ -1,5 +1,7 @@
 class Backoffice::AdminsController < BackofficeController
   before_action :set_admin, only: [:edit, :update, :destroy]
+  after_action :verify_authorized, only: [:new, :destroy]
+  after_action :verify_policy_scoped, only: :index
 
 
   def index
@@ -41,6 +43,7 @@ class Backoffice::AdminsController < BackofficeController
   end
 
   def destroy
+    authorize @admin
     admin_email = @admin.email
 
     if @admin.destroy
