fa2b0968ec50e4be2c4a6ef83b8a0aab9d951e23
Pedro Ivo <pedryvo@gmail.com>
2022-01-17 21:29:32 -0300

    config modal JS

--- app/controllers/backoffice/send_mail_controller.rb
+++ app/controllers/backoffice/send_mail_controller.rb
@@ -1,2 +1,20 @@
 class Backoffice::SendMailController < ApplicationController
+  before_action :set_admin, only: :edit
+
+
+  def edit
+    respond_to do |format|
+      format.js {  }
+    end
+  end
+
+  def create
+    
+  end
+
+  private
+
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   namespace :backoffice do
     resources :categories, except: [:show]
     resources :admins, except: [:show]
+    resources :send_mail, only: [:edit, :create]
     get 'dashboard', to: 'dashboard#index'
   end
 
