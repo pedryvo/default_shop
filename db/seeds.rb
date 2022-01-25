023ea1dcc8d2b424b93188cd945192724a6038ec
Pedro Ivo <pedryvo@gmail.com>
2022-01-25 19:50:03 -0300

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,8 @@ Admin.create!(email: 'admin@admin.com', name: 'Administrador Geral', password: '
 10.times do
   Admin.create!(email: Faker::Internet.email, name: Faker::Name.name_with_middle, password: '12345678', password_confirmation: '12345678', role: [0,1].sample)
 end
+
+# Ads
+100.times do
+  Ad.create!(title: Faker::Lorem.sentence, description: Faker::Lorem.paragraph, member: Member.all.sample, category: Category.all.sample)
+end
